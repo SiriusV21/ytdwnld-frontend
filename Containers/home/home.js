6da4329cbda79313b14811f03d9623cd4dcb0e4f
Siddharth <siddharth.verman@gmail.com>
2020-05-08 16:04:03 +0530

    uploading project

---
+++ Containers/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+
+
+const home = () => {
+	<div>
+		<h1 className=''> Youtube Downloader </h1>
+		<h2 className=''> blah blah blah..... </h2>
+		<input type='button' value='Go to Downloader'>
+	</div>
+
+}
+
+export default home;
